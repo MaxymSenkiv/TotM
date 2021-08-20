0cb885bf125552a862f8e347b611fb4830de7df6
MaxymSenkiv <maksym.senkiv.knm.2019@lpnu.ua>
2021-08-20 20:02:27 +0300

    Enemy
    Moving enemy

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private Vector3 _start_position;
+    [SerializeField] private Vector3 _end_position;
+    [SerializeField] private float _speed;
+    private bool _can_move = true;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _start_position = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_can_move == true)
+            transform.position = Vector3.MoveTowards(transform.position, _end_position, _speed * Time.deltaTime);
+        if (transform.position == _end_position)
+        {
+            _end_position = _start_position;
+            _start_position = transform.position;
+            StartCoroutine("EnemyStay");
+        }
+    }
+
+    IEnumerator EnemyStay()
+    {
+        _can_move = false;
+        yield return new WaitForSeconds(1f);
+        _can_move = true;
+        yield return null;
+    }
+}
